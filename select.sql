ee6541bc689ef758bbf8a03eb73092dd583c35de
Gustavo Rocha <gurocha05@gmail.com>
2025-03-05 17:34:41 -0300

    simple select

---
+++ select.sql
@@ -0,0 +1,19 @@
+-- Create a simple table
+-- Put "DROP TABLE" in the begining, just in case
+DROP TABLE clientes
+CREATE TABLE clientes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    cidade VARCHAR(50) NOT NULL
+);
+-- Put some elements in the table
+INSERT INTO clientes (nome, email, cidade) VALUES
+('Ana Souza', 'ana@email.com', 'São Paulo'),
+('Carlos Lima', 'carlos@email.com', 'Rio de Janeiro'),
+('Mariana Rocha', 'mariana@email.com', 'São Paulo'),
+('João Pedro', 'joao@email.com', 'Belo Horizonte');
+-- Now select them using "SELECT" and select all using "*"
+SELECT * FROM clientes
+-- Drop the table in the final line
+DROP TABLE clientes
